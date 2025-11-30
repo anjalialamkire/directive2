3f529bf5413da1284bf76940e35a9e90191e0e1d
anjalialamkire <anjali.alamkire21@gmail.com>
2025-11-30 13:56:26 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CssHighlighterDirective } from './shared/component/directive/cssHighlighter.directive';
+import { RendrerCssHighDirective } from './shared/component/directive/rendrer-css-high.directive';
+import { ToLowercaseDirective } from './shared/component/directive/to-lowercase.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CssHighlighterDirective,
+    RendrerCssHighDirective,
+    ToLowercaseDirective
+  
   ],
   imports: [
     BrowserModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
